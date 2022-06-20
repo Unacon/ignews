dcc7327e617eaa1dd1ad15e6105c24b1340386b4
Joao Vitor Pinheiro <joaovitor_892@hotmail.com>
2022-06-20 20:10:30 -0300

    Finalizando estilos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import syles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicío | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h2>
-        Hello <span>Word</span>
-      </h2>
+
+      <main className={syles.contentContainer}>
+        <section className={syles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
